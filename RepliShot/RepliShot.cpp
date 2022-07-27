9eeb1bb2892ca3fd5b7d5f5c5ebeb0dc9275945f
Zaren <adexoo@hotmail.com>
2022-07-27 08:41:03 -0600

    commented out some debug code which can also be used to get the club reading data.

--- RepliShot/RepliShot.cpp
+++ RepliShot/RepliShot.cpp
@@ -1041,87 +1041,87 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
         int height = 150;
         int width = 305;
 
-        //Capture part of screen
-        HDC desktop_hdc = GetDC(NULL); // get the desktop device context
-        HDC hDest = CreateCompatibleDC(desktop_hdc); // create a device context to use yourself
-        
-        //define portion of the screen
-        RECT capture_area;
-        HWND SomeWindowHandle = GetDesktopWindow();
-        GetWindowRect(SomeWindowHandle, &capture_area);
-        capture_area.left = capture_area.right * 4 / 5;
-        capture_area.top = capture_area.bottom * 0.15;
-        capture_area.bottom = capture_area.bottom * 0.19;
-
-        int x, y;
-        int capture_width = capture_area.right - capture_area.left;
-        int capture_height = capture_area.bottom - capture_area.top;
-
-        BYTE* bitPointer;
-        BITMAPINFO biInfo;
-        biInfo.bmiHeader.biSize = sizeof(biInfo.bmiHeader);
-        biInfo.bmiHeader.biWidth = capture_width;
-        biInfo.bmiHeader.biHeight = capture_height;
-        biInfo.bmiHeader.biPlanes = 1;
-        biInfo.bmiHeader.biBitCount = 32;
-        biInfo.bmiHeader.biCompression = BI_RGB;
-        biInfo.bmiHeader.biSizeImage = capture_width * 4 * capture_height;
-        biInfo.bmiHeader.biClrUsed = 0;
-        biInfo.bmiHeader.biClrImportant = 0;
-        HBITMAP hBitmap2 = CreateDIBSection(hDest, &biInfo, DIB_RGB_COLORS, (void**)(&bitPointer), NULL, NULL);
-        SelectObject(hDest, hBitmap2);
-        BitBlt(hDest, 0, 0, capture_width, capture_height, desktop_hdc, capture_area.left, capture_area.top, SRCCOPY);
-
-        int bin_size = (capture_area.bottom - capture_area.top) / (HBINS - 1);
-        int histogram[HBINS][VBINS];
-        int intensities[3][5];
-        for (x = 0; x < VBINS; x++) {
-            for (y = 0; y < HBINS; y++) {
-                histogram[y][x] = 0;
-            }
-        }
-        for (x = 0; x < (capture_width * 4 * capture_height); x += 4) {
-            if ((bitPointer[x] > THRESHOLD) && (bitPointer[x + 1] > THRESHOLD) && (bitPointer[x + 2] > THRESHOLD)) {
-                histogram[((x / 4) / capture_width) / bin_size][((x / 4) % capture_width) / bin_size]++;
-            }
-        }
-        int best_error = -1;
-        int picked_club = -1;
-        std::ostringstream select_club;
-        for (int z = 0; z < CLUBS; z++) {
-            int total_error = 0;
-            for (x = 0; x < VBINS; x++) {
-                for (y = 0; y < HBINS; y++) {
-                    total_error += abs(data::club_image_data_1080p[z][y][x] - histogram[y][x]);
-                }
-            }
-            if (best_error < 0 || total_error < best_error) {
-                best_error = total_error;
-                picked_club = z;
-                select_club.str("");
-                select_club << "Club " << (int)z << " Selected! Error: " << total_error << "\n";
-            }
-        }
-        OutputDebugStringA(select_club.str().c_str());
-
-        if (best_error > MINCLUBCHANGE) picked_club = -1;
-
-        std::ostringstream stros;
-        stros << "New Data!\n{ {";
-        for (y = 0; y < HBINS; y++) {
-            for (x = 0; x < VBINS; x++) {
-                stros << histogram[y][x];
-                stros << ", ";
-            }
-            if (y < HBINS - 1) stros << "},\n{";
-            else stros << "} }\n";
-        }
-        OutputDebugStringA(stros.str().c_str());
-        
-        BitBlt(hdc, left, top, capture_area.right - capture_area.left, capture_area.bottom - capture_area.top, desktop_hdc, capture_area.left, capture_area.top, SRCCOPY);
-        
-        ReleaseDC(NULL, desktop_hdc);
-        DeleteDC(hDest);
+        ////Capture part of screen
+        //HDC desktop_hdc = GetDC(NULL); // get the desktop device context
+        //HDC hDest = CreateCompatibleDC(desktop_hdc); // create a device context to use yourself
+        //
+        ////define portion of the screen
+        //RECT capture_area;
+        //HWND SomeWindowHandle = GetDesktopWindow();
+        //GetWindowRect(SomeWindowHandle, &capture_area);
+        //capture_area.left = capture_area.right * 4 / 5;
+        //capture_area.top = capture_area.bottom * 0.15;
+        //capture_area.bottom = capture_area.bottom * 0.19;
+        //
+        //int x, y;
+        //int capture_width = capture_area.right - capture_area.left;
+        //int capture_height = capture_area.bottom - capture_area.top;
+        //
+        //BYTE* bitPointer;
+        //BITMAPINFO biInfo;
+        //biInfo.bmiHeader.biSize = sizeof(biInfo.bmiHeader);
+        //biInfo.bmiHeader.biWidth = capture_width;
+        //biInfo.bmiHeader.biHeight = capture_height;
+        //biInfo.bmiHeader.biPlanes = 1;
+        //biInfo.bmiHeader.biBitCount = 32;
+        //biInfo.bmiHeader.biCompression = BI_RGB;
+        //biInfo.bmiHeader.biSizeImage = capture_width * 4 * capture_height;
+        //biInfo.bmiHeader.biClrUsed = 0;
+        //biInfo.bmiHeader.biClrImportant = 0;
+        //HBITMAP hBitmap2 = CreateDIBSection(hDest, &biInfo, DIB_RGB_COLORS, (void**)(&bitPointer), NULL, NULL);
+        //SelectObject(hDest, hBitmap2);
+        //BitBlt(hDest, 0, 0, capture_width, capture_height, desktop_hdc, capture_area.left, capture_area.top, SRCCOPY);
+        //
+        //int bin_size = (capture_area.bottom - capture_area.top) / (HBINS - 1);
+        //int histogram[HBINS][VBINS];
+        //int intensities[3][5];
+        //for (x = 0; x < VBINS; x++) {
+        //    for (y = 0; y < HBINS; y++) {
+        //        histogram[y][x] = 0;
+        //    }
+        //}
+        //for (x = 0; x < (capture_width * 4 * capture_height); x += 4) {
+        //    if ((bitPointer[x] > THRESHOLD) && (bitPointer[x + 1] > THRESHOLD) && (bitPointer[x + 2] > THRESHOLD)) {
+        //        histogram[((x / 4) / capture_width) / bin_size][((x / 4) % capture_width) / bin_size]++;
+        //    }
+        //}
+        //int best_error = -1;
+        //int picked_club = -1;
+        //std::ostringstream select_club;
+        //for (int z = 0; z < CLUBS; z++) {
+        //    int total_error = 0;
+        //    for (x = 0; x < VBINS; x++) {
+        //        for (y = 0; y < HBINS; y++) {
+        //            total_error += abs(data::club_image_data_1080p[z][y][x] - histogram[y][x]);
+        //        }
+        //    }
+        //    if (best_error < 0 || total_error < best_error) {
+        //        best_error = total_error;
+        //        picked_club = z;
+        //        select_club.str("");
+        //        select_club << "Club " << (int)z << " Selected! Error: " << total_error << "\n";
+        //    }
+        //}
+        //OutputDebugStringA(select_club.str().c_str());
+        //
+        //if (best_error > MINCLUBCHANGE) picked_club = -1;
+        //
+        //std::ostringstream stros;
+        //stros << "New Data!\n{ {";
+        //for (y = 0; y < HBINS; y++) {
+        //    for (x = 0; x < VBINS; x++) {
+        //        stros << histogram[y][x];
+        //        stros << ", ";
+        //    }
+        //    if (y < HBINS - 1) stros << "},\n{";
+        //    else stros << "} }\n";
+        //}
+        //OutputDebugStringA(stros.str().c_str());
+        //
+        //BitBlt(hdc, left, top, capture_area.right - capture_area.left, capture_area.bottom - capture_area.top, desktop_hdc, capture_area.left, capture_area.top, SRCCOPY);
+        //
+        //ReleaseDC(NULL, desktop_hdc);
+        //DeleteDC(hDest);
 
         //Draw Golf Ball
         hdcMem = CreateCompatibleDC(hdc);
